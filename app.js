2643a20d3049569a496c71a43f92f86630be43de
Zemer Shuker <zemersh@gmail.com>
2019-12-08 17:57:06 +0000

    adding TimeIPWebApp repository

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+var os = require('os');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/time',function(req,res){
+  res.sendFile(path.join(__dirname+'/templates/time.html'));
+});
+
+router.get('/address',function(req,res){
+  var addresses = getIpAddress();
+  res.sendFile(path.join(__dirname+'/templates/address.html'));
+  res.send(`${addresses}`);
+});
+
+function getIpAddress(){
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
+    }
+    return addresses;
+}
+
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 8080);
+
+console.log('Running at Port 8080');
